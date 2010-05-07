bc56c961f28763b68569647e9fc31e5f88444686
Jingfei.Hu@gmail.com <Jingfei.Hu@gmail.com@3a95c3f6-2b41-11df-be6c-f52728ce0ce6>
2010-05-07 09:42:49 +0000

    1. Change the protocol to be UDP

--- GT/func/NetSvr.h
+++ GT/func/NetSvr.h
@@ -1,8 +1,8 @@
-
 #ifndef AN_NETSVR_H_ 
 #define AN_NETSVR_H_
 
 #include <winsock.h>
+
 #define socklen_t int
 #define RECVBUFLEN  10240
 
@@ -18,5 +18,25 @@ public:
 	int InitNet();
 	virtual void OnDataRecv() = 0;
 };
+
+/*
+ * A UDP socket server
+ */
+class CNetSvrUdp  
+{
+public:
+	CNetSvrUdp();
+	virtual ~CNetSvrUdp();
+	unsigned char recvbuf[RECVBUFLEN];
+
+	int recvlen;
+	int port;
+	
+	bool StartSvr();	
+	// When a diagram arrives, this method is invoked.
+	virtual void OnDataRecv(void) = 0;
+	int InitNet();
+};
+
 
 #endif 
